c043ce19022469ff6b1b7396be34a7eeb407d34e
dvanhoang100904 <daovanhoang100904@gmail.com>
2025-04-17 17:18:06 +0700

    update template the customer

--- resources/views/customer/layouts/content.blade.php
+++ resources/views/customer/layouts/content.blade.php
@@ -6,6 +6,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>@yield('title', 'Trang Chủ')</title>
+
+    {{-- bootstrap css --}}
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-SgOJa3DmI69IUzQ2PVdRZhwQ+dy64/BUtbMJw1MZ8t5HZApcHrRKUc4W0kG879m7" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+
 </head>
 
 <body>
@@ -21,6 +28,13 @@
 
     {{-- footer --}}
     @include('customer.layouts.footer')
+
+    {{-- bootstrap js --}}
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-k6d4wzSIapyDyv1kpU366/PK5hCdSbCRGRCMv+eplOQJWyd1fbcAu9OCUj5zNLiq" crossorigin="anonymous">
+    </script>
+
+    <script src="{{ asset('js/script.js') }}"></script>
 </body>
 
 </html>
