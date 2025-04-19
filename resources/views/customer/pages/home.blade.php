1d0c82161f1996c47f8d345d6b2b66c3c500bf2a
V-nlkt <bachlongvu357@gmail.com>
2025-04-19 10:07:06 +0700

    update home

--- resources/views/customer/pages/home.blade.php
+++ resources/views/customer/pages/home.blade.php
@@ -1,5 +1,48 @@
 @extends('customer.layouts.content')
 @section('title', 'Trang chủ')
-@section('content')
+@section('content')<div class="container">
 
+{{-- Banner đầu trang --}}
+<div class="banner-wrapper">
+    <img src="{{ asset('images/banner1.png') }}" alt="Banner" class="banner-img">
+</div>
+
+{{-- Khu vực sản phẩm nổi bật --}}
+<section class="featured-products">
+    <h2>SẢN PHẨM NỔI BẬT</h2>
+
+    <div class="product-grid">
+        {{-- Hiển thị sản phẩm nổi bật --}}
+        @foreach($featuredProducts as $product)
+            <a href="#" class="product-card">
+                <div class="product-image">
+                    {{-- Ảnh sản phẩm --}}
+                    <img src="{{ asset($product->image) }}" alt="{{ $product->product_name }}">
+                </div>
+
+                <div class="product-info">
+                    {{-- Tên sản phẩm --}}
+                    <h3>{{ $product->product_name }}</h3>
+
+                    {{-- Giá sản phẩm --}}
+                    <p class="price">{{ number_format($product->price) }}₫</p>
+
+                    {{-- Mô tả ngắn --}}
+                    <p class="description">{{ $product->description }}</p>
+
+                    {{-- Nút yêu thích --}}
+                    <button class="like-btn">❤</button>
+
+                    {{-- Nút Thêm vào giỏ --}}
+                    <form method="POST" action="#">
+                        @csrf
+                        <button type="submit" class="add-to-cart-btn">Thêm vào giỏ</button>
+                    </form>
+                </div>
+            </a>
+        @endforeach
+    </div>
+</section>
+
+</div>
 @endsection
