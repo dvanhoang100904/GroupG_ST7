56a5848353dde9610daae34def596be44545af52
V-nlkt <bachlongvu357@gmail.com>
2025-04-26 10:03:39 +0700

    update

--- resources/views/customer/layouts/footer.blade.php
+++ resources/views/customer/layouts/footer.blade.php
@@ -1,4 +1,40 @@
-   <!-- Footer -->
-   <footer class="footer">
+<!-- Footer -->
+<footer class="footer">
+  <div class="footer-container">
+    <div class="footer-column">
+      <h4>Về chúng tôi</h4>
+      <p>GroupG - Trang thương mại điện tử chuyên cung cấp sản phẩm chất lượng với giá cả hợp lý.</p>
+    </div>
+
+    <div class="footer-column">
+      <h4>Liên kết nhanh</h4>
+      <ul>
+        <li><a href="{{ route('home') }}">Trang chủ</a></li>
+        <li><a href="{{ route('products.index') }}">Sản phẩm</a></li>
+        <li><a href="#">Giới thiệu</a></li>
+        <li><a href="#">Liên hệ</a></li>
+      </ul>
+    </div>
+
+    <div class="footer-column">
+      <h4>Hỗ trợ khách hàng</h4>
+      <ul>
+        <li><a href="#">Chính sách bảo hành</a></li>
+        <li><a href="#">Chính sách bảo mật</a></li>
+      </ul>
+    </div>
+
+    <div class="footer-column">
+      <h4>Kết nối với chúng tôi</h4>
+      <div class="social-icons">
+         <a href="https://www.facebook.com" target="_blank"><i class="fab fa-facebook-f"></i></a>
+         <a href="https://www.instagram.com" target="_blank"><i class="fab fa-instagram"></i></a>
+         <a href="https://www.twitter.com" target="_blank"><i class="fab fa-twitter"></i></a>
+      </div>
+    </div>
+  </div>
+
+  <div class="footer-bottom">
     <p>&copy; 2025 - GroupG | Trang web thương mại điện tử</p>
-    </footer>
+  </div>
+</footer>
