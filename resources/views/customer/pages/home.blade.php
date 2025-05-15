f409421e9db5c69b9148fe392b6d58296bd7a723
V-nlkt <bachlongvu357@gmail.com>
2025-05-15 19:58:55 +0700

    update cuoi cung

--- resources/views/customer/pages/home.blade.php
+++ resources/views/customer/pages/home.blade.php
@@ -5,6 +5,11 @@
     {{-- Bắt đầu nội dung chính sẽ được chèn vào layout --}}
     <div class="container">
 
+    {{-- Banner đầu trang --}}
+    <div class="banner-wrapper">
+        <img src="{{ asset('images/banner1.png') }}" alt="Banner" class="banner-img">
+    </div>
+
         {{-- Banner đầu trang --}}
         <div class="banner-wrapper">
             @foreach ($slides as $slide)
